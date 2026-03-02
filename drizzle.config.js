9f59bfe64a1abdf6f51002b323ef17eabab0c7e2
Vraj Panchal <vrajpanchal110106@gmail.com>
2026-03-02 21:48:33 +0530

    Update Drizzle and DB config for Render compatibility

--- drizzle.config.js
+++ drizzle.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     },
     verbose: true,
     strict: true,
--- src/db/db.js
+++ src/db/db.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export const db = drizzle(pool);
